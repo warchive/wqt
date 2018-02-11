651b993b35e50577af38fb8b6c5f41cc91825728
Deep Dhillon <deep@deepdhillon.ca>
2018-02-10 21:38:06 -0500

    added a way to get all the resource files

--- wqt/templates/files.py
+++ wqt/templates/files.py
@@ -3,7 +3,8 @@
 from wqt.utils.helper import (
     OS,
     get_wqt_path,
-    get_platform
+    get_platform,
+    get_files_recursively
 )
 
 
@@ -40,7 +41,7 @@ def __get_cmake_console():
     return console_path + '/CMakeLists.txt'
 
 
-def get_cmake(qt_type):
+def get_cmake_file(qt_type):
     """Returns the path for cmake template file for the qt type specified"""
 
     os = get_platform()
@@ -54,3 +55,44 @@ def get_cmake(qt_type):
     else:
         raise ValueError('Type of Qt application not valid')
 
+
+def get_config_file():
+    """Returns the config file based on the OS"""
+
+    os = get_platform()
+    config_path = get_wqt_path() + '/templates/config'
+
+    if os == OS.mac:
+        return config_path + '/osx/config.json'
+    else:
+        return config_path + '/others/config.json'
+
+
+def get_src_files(qt_type):
+    """Returns the src files for the application type"""
+
+    application_path = get_wqt_path() + '/templates/applications'
+
+    if qt_type == QType.QUICK:
+        return get_files_recursively(application_path + '/quick/src')
+    elif qt_type == QType.WIDGETS:
+        return get_files_recursively(application_path + '/widgets/src')
+    elif qt_type == QType.CONSOLE:
+        return get_files_recursively(application_path + '/console/src')
+    else:
+        raise ValueError('Type of Qt application not valid')
+
+
+def get_res_files(qt_type, ext):
+    """Returns the resource files for the application type"""
+
+    application_path = get_wqt_path() + '/templates/applications'
+
+    if qt_type == QType.QUICK:
+        return get_files_recursively(application_path + '/quick/res', ext)
+    elif qt_type == QType.WIDGETS:
+        return get_files_recursively(application_path + '/widgets/res', ext)
+    elif qt_type == QType.CONSOLE:
+        return get_files_recursively(application_path + '/console/res', ext)
+    else:
+        raise ValueError('Type of Qt application not valid')
